1605732093fb3a5cf6ba3f94c463471de5c3eaa9
Michal <michal.rosiol@gmail.com>
2021-05-17 11:47:27 +0200

    update lab5

--- labs5/ex1.py
+++ labs5/ex1.py
@@ -2,7 +2,7 @@ import requests
 import io
 import numpy as np
 import matplotlib.pyplot as plt
-
+from itertools import combinations
 
 response = requests.get('https://wp.faculty.wmi.amu.edu.pl/daneX.npy')
 response.raise_for_status()
@@ -16,9 +16,14 @@ gridy = np.linspace(min(y), max(y), 100)
 
 H, xedges, yedges = np.histogram2d(x, y, bins=[gridx, gridy])
 
-ax = plt.subplot(111)
+ax = plt.subplot(111, sharex=[0,100], sharey=[0,100])
 X, Y = np.meshgrid(xedges, yedges)
 ax.pcolormesh(X, Y, H)
+
+
+for pair in combinations(X,2):
+    assert np.array_equal(pair[0],pair[1])
+
 
 plt.show()
 
